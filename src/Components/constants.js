faaa673b83c9883241f1c3d109b6f9663e58300a
akudahewa <akudahewa@gmail.com>
2022-01-15 01:13:10 +0530

    fix bookingPage loading issues

--- src/Components/constants.js
+++ src/Components/constants.js
@@ -1,5 +1,5 @@
 
-export const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvbmxpbmUiLCJyb2xlcyI6WyJPTkxJTkUiLCJTVEFGRiJdLCJpc3MiOiIvbG9naW4iLCJleHAiOjE2NDIwOTM4MTV9.V5XcccKpfzpy1ZWyq1HiBgT6U47YXREwS8QxpzEMxvc";
+export const ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvbmxpbmUiLCJyb2xlcyI6WyJPTkxJTkUiLCJTVEFGRiJdLCJpc3MiOiIvbG9naW4iLCJleHAiOjE2NDIyNzM3NTV9.zLEr3LSoEhQgK7JVkY-0p1RswiS_sws4Ol52LCN1rU8";
 export const BASE_URL = "http://ec2-3-6-92-221.ap-south-1.compute.amazonaws.com:8081/v1";
 
 
